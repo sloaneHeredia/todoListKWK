071c57277a1024c8b3761e209e7c1b4cde72fda0
Sloane Heredia <sloane.heredia@gmail.com>
2022-06-28 10:25:19 -0500

    Initial Commit

---
+++ todoListKWK/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  todoListKWK
+//
+//  Created by Sloane Heredia on 6/27/22.
+//
+
+import UIKit
+
+
+class ToDo {
+    var name = ""
+    var important = false 
+}
---
+++ todoListKWK/ToDoTableViewController ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ToDoTableViewController ViewController.swift
+//  todoListKWK
+//
+//  Created by Sloane Heredia on 6/27/22.
+//
+
+import UIKit
+
+class ToDoTableViewController_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
